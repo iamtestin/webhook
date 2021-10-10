2b3887c095295089ca4de1b23f6c4ba97f394fa3
flim <francis.lim@mlb.com>
2021-10-09 21:18:58 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/google/go-github/github"
+)
+
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	payload, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("error reading request body: err=%s\n", err)
+		return
+	}
+	defer r.Body.Close()
+	event, err := github.ParseWebHook(github.WebHookType(r), payload)
+	if err != nil {
+		log.Printf("could not parse webhook: err=%s\n", err)
+		return
+	}
+	fmt.Printf("Event: %v\n", event)
+
+	switch e := event.(type) {
+	case *github.PingEvent:
+		w.Write([]byte("PONG!"))
+	case *github.PushEvent:
+		// this is a commit push, do something with it
+	case *github.PullRequestEvent:
+		// this is a pull request, do something with it
+	case *github.WatchEvent:
+		// https://developer.github.com/v3/activity/events/types/#watchevent
+		// someone starred our repository
+		fmt.Printf("Event: %v\n", e)
+		if e.Action != nil && *e.Action == "starred" {
+			fmt.Printf("%s starred repository %s\n",
+				*e.Sender.Login, *e.Repo.FullName)
+		}
+	default:
+		log.Printf("unknown event type %s\n", github.WebHookType(r))
+		return
+	}
+	w.Write([]byte("OK"))
+}
+
+func main() {
+	http.HandleFunc("/webhook", ServeHTTP)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+}
